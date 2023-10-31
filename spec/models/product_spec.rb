e0d4042077d3c9ad0cff0a2b179fe26e4b10ee38
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-10-31 12:39:02 -0700

    Add product tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    
+    before do
+      @category = Category.new(name: 'Flowers')
+      @category.save
+    end
+
+    it 'saves a product successfully with all four fields set' do
+      @product = Product.new(
+        name: 'Corpse Flower',
+        price_cents: 999,
+        quantity: 10,
+        category: @category
+      )
+      expect(@product).to be_valid
+    end
+
+    it 'requires name to be set' do
+      @product = Product.new(
+        name: nil,
+        price_cents: 999,
+        quantity: 10,
+        category: @category
+      )
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'requires price to be set' do
+      @product = Product.new(
+        name: 'Corpse Flower',
+        price_cents: nil,
+        quantity: 10,
+        category: @category
+      )
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'requires quantity to be set' do
+      @product = Product.new(
+        name: 'Corpse Flower',
+        price_cents: 999,
+        quantity: nil,
+        category: @category
+      )
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'requires category to be set' do
+      @product = Product.new(
+        name: 'Corpse Flower',
+        price_cents: 999,
+        quantity: 10,
+        category: nil
+      )
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+  end
+end
