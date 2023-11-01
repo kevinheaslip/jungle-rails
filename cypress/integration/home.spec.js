9a2171a8c5ae61dc6220a706e19d515365914d44
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-11-01 12:54:58 -0700

    Add Cypress home page feature spec

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+describe('Home', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  
+  it('Loads the home page', () => {
+    cy.visit('/');
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });  
+});
